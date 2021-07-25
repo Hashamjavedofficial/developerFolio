61a32f49f6469e16928d0abd3653a096481ee02b
Hasham Javed <hasham.hasham1@gmail.com>
2021-07-25 19:25:34 +0500

    working on meida link

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,10 +11,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Hasham Javed",
+  title: "Hi all, I'm Hasham Javed",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 having 5 years of experience in building Web applications with JavaScript / Reactjs / Nodejs / Vuejs  and many other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
@@ -25,11 +25,10 @@ const greeting = {
 
 const socialMediaLinks = {
   github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
+  linkedin: "https://www.linkedin.com/in/hashamjaved/",
+  gmail: "hasham.hasham1@gmail.com",
+  twitterDetails:'duahjsgdhjgashjdg',
   facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
   stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
